8c1f40a5e52a9915fca562e0df8f080acfd69742
Camelia <Cameliaafolabi@gmail.com>
2025-01-21 11:18:22 +0100

    updated the title

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,31 +1,31 @@
-import { useState } from "react"
-
-
+import { useState } from "react";
 
 const Counter = () => {
-    const [step, setStep]= useState(0)
-    const [count, setCount]= useState(0)
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
 
-    function handleStepSum (){
-        if(step >= 0)setStep((prevVal)=> prevVal + 1)
-       
-    }
-    function handleStepSub (){
-        if(step > 0)setStep((prevVal)=> prevVal - 1)
-        
-    }
-    function handleCounterSum(){
-        if(count >= 0)setCount((preVal)=> preVal + step)
-    }
-    function handleCounterSub(){
-        if(count > 0)setCount((preVal)=> preVal - step)
-    }
+  function handleStepSum() {
+    if (step >= 0) setStep((prevVal) => prevVal + 1);
+  }
+  function handleStepSub() {
+    if (step > 0) setStep((prevVal) => prevVal - 1);
+  }
+  function handleCounterSum() {
+    if (count >= 0) setCount((preVal) => preVal + step);
+  }
+  function handleCounterSub() {
+    if (count > 0) setCount((preVal) => preVal - step);
+  }
+  function handleReset() {
+    setCount(0);
+    setStep(0);
+  }
 
-const today = new Date(); // Get the current date and time
+  const today = new Date(); // Get the current date and time
 
   // Calculate the future/past date by adding/subtracting days in milliseconds
   const futureDate = new Date(today.getTime() + count * 24 * 60 * 60 * 1000);
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const options = { year: "numeric", month: "long", day: "numeric" };
   const formattedToday = today.toLocaleDateString(undefined, options);
   const formattedFutureDate = futureDate.toLocaleDateString(undefined, options);
 
@@ -33,21 +33,57 @@ const today = new Date(); // Get the current date and time
   const timeDirection = count >= 0 ? "future" : "past";
   return (
     <div className="bg-blue-400 text-center p-8 max-w-96  m-auto rounded-2xl mt-8   text-blue-100">
-    <div className="flex flex-row gap-5 items-center justify-between">
-    <button onClick={handleStepSub} className="px-2 bg-blue-500 text-white text-lg">-</button>
+      <div className="flex flex-row gap-5 items-center justify-between">
+        <button
+          onClick={handleStepSub}
+          className="px-2 bg-blue-500 text-white text-lg"
+        >
+          -
+        </button>
         <p>Step: {step}</p>
-       
-        <button onClick={handleStepSum} className="px-2 bg-blue-500 text-white text-lg">+</button>
-    </div>
-    <div className="flex flex-row gap-5 items-center justify-between mt-6 text-blue-900">
-    <button onClick={handleCounterSub} className="px-2 bg-blue-500 text-white text-lg">-</button>
-        <p >Count: {count}</p>
-        <button onClick={handleCounterSum} className="px-2 bg-blue-500 text-white text-lg">+</button>
-    </div>
-        <p className="mt-6">Today is {formattedToday}. </p>
-        <p className="mt-6">The {timeDirection} date {Math.abs(count)} days from today is {formattedFutureDate}.</p>
-    </div>
-  )
-}
 
-export default Counter
+        <button
+          onClick={handleStepSum}
+          className="px-2 bg-blue-500 text-white text-lg"
+        >
+          +
+        </button>
+      </div>
+      <div className="flex flex-row gap-5 items-center justify-between mt-6 text-blue-900">
+        <button
+          onClick={handleCounterSub}
+          className="px-2 bg-blue-500 text-white text-lg"
+        >
+          -
+        </button>
+        <p>Count: {count}</p>
+        <button
+          onClick={handleCounterSum}
+          className="px-2 bg-blue-500 text-white text-lg"
+        >
+          +
+        </button>
+      </div>
+      {count >= 1 && (
+        <div className="mb-4">
+          <p className="mt-6">Today is {formattedToday}. </p>
+          <p className="mt-6">
+            The {timeDirection} date {Math.abs(count)} days from today is{" "}
+            {formattedFutureDate}.
+          </p>
+        </div>
+      )}
+
+      <div className="py-5">
+        <button
+          onClick={handleReset}
+          className=" m-auto bg-white text-blue-900 p-4 rounded-2xl"
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
